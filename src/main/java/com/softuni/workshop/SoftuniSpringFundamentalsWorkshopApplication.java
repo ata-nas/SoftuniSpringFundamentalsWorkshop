3150ac503a10fa8cf33882d4a82364b97487b9ce
ATANAS ATANASOV <94196097+ata-nas@users.noreply.github.com>
2023-03-01 10:31:33 +0200

    adding gradle and updating gitignore

---
+++ src/main/java/com/softuni/workshop/SoftuniSpringFundamentalsWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.softuni.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftuniSpringFundamentalsWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftuniSpringFundamentalsWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softuni/workshop/SoftuniSpringFundamentalsWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftuniSpringFundamentalsWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
